afdcc5966a8777e2654c4fca27c907b134159f25
EDUARDO VINICIUS ANDRUCZEWICZ <eduardo.andruczewic@alunos.sc.senac.br>
2021-09-01 17:19:18 -0300

    Upando Arquivos

--- jogo-velha/src/Componentes/Board/index.js
+++ jogo-velha/src/Componentes/Board/index.js
@@ -1,25 +1,45 @@
-class Board extends React.Component {
+import React from "react"
+import { Square } from "../Square"
+
+
+export class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
+
+
     renderSquare(i) {
-        return <Square value={i} />;
-    }
+        return (
+        <Square 
+         value={this.props.squares[i]}
+         onClick={() => this.props.onClick(i)} 
+         />
+        );    
+    }   
 
     render() {
-        const status = 'Next player: X';
-
         return (
-            <div>
-                <div className="status">{status}</div>
+            <div>                
                 <div className="board-row">
-                  {this.renderSquare(0)}
-                  {this.renderSquare(1)}
-                  {this.renderSquare(2)} 
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
-        )
+        );
     }
 }
